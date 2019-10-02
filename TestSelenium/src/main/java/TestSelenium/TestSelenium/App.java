1aeb58afc918af51200129edb7060623004d30e6
vikeshpawar <vikeshpawar@DESKTOP-M0K5BIV>
2019-10-02 15:03:41 +0530

    FIrst Ever Commit

---
+++ TestSelenium/src/main/java/TestSelenium/TestSelenium/App.java
@@ -0,0 +1,13 @@
+package TestSelenium.TestSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestSelenium/src/main/java/TestSelenium/TestSelenium/TestClass.java
@@ -0,0 +1,10 @@
+package TestSelenium.TestSelenium;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
